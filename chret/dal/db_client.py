9bf90f60c5ad9c8c45bc4fee417d2865f74b3f92
ben <ben.gabay38@gmail.com>
2020-06-08 23:45:24 +0300

    add get chrome downloads to dal

--- chret/dal/db_client.py
+++ chret/dal/db_client.py
@@ -1,7 +1,20 @@
 from chret.dal.db_connection import DBConnection
+from chret.dal.models.Download import Download
 from chret.dal.models.Login import Login
 
 
 class DBClient(DBConnection):
     def get_chrome_credentials(self):
         return self.select(Login, serializable=True)
+
+    def import_chrome_credentials(self, credentials):
+        pass
+
+    def get_chrome_history(self):
+        pass
+
+    def get_chrome_top_sites(self):
+        pass
+
+    def get_chrome_downloads(self):
+        return self.select(Download, serializable=True)

eb75c0ef1acec48d371ac624e6633ff478f96697
ben <ben.gabay38@gmail.com>
2020-07-24 14:49:10 +0300

    implement json file adapter

--- chromesy/file_adapters/json_adapter.py
+++ chromesy/file_adapters/json_adapter.py
@@ -1,9 +1,13 @@
+import json
+
+from chromesy.config import WRITE_FILE_MODE
 from chromesy.interfaces.IFileAdapter import IFileAdapter
 
 
 class JsonFileAdapter(IFileAdapter):
     def write(self, data, output_file_path):
-        pass
+        with open(output_file_path, WRITE_FILE_MODE) as output_file:
+            json.dump(data, output_file)
 
     def read(self, path):
-        pass
+        return json.load(path)

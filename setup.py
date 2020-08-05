26e5fb5ee3a63679085dda827a5792c6d853e32e
ben <ben.gabay38@gmail.com>
2020-08-05 21:57:11 +0300

    fix packages field in setup file

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bengabay11/chromesy",
-    packages=setuptools.find_packages("chromesy"),
+    packages=setuptools.find_packages(),
     install_requires=[
         "sqlalchemy==1.3.18"
     ],

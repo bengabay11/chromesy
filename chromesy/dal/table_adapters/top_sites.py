876b7ff57a50ee43080cfb78a85de6ed73c53825
ben <ben.gabay38@gmail.com>
2020-08-26 22:57:11 +0300

    add DI for serializable param

--- chromesy/dal/table_adapters/top_sites.py
+++ chromesy/dal/table_adapters/top_sites.py
@@ -3,5 +3,5 @@ from chromesy.dal.models.TopSite import TopSite
 
 
 class TopSitesTableAdapter(DBConnection):
-    def get_top_sites(self):
-        return self.select(TopSite, serializable=True)
+    def get_top_sites(self, serializable=False):
+        return self.select(TopSite, serializable=serializable)

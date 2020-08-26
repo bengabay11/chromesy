1b67d98982ae363e94ad3d343023a59d30e231cb
ben <ben.gabay38@gmail.com>
2020-08-26 22:56:55 +0300

    fix / and the start in the path utility

--- chromesy/path.py
+++ chromesy/path.py
@@ -5,19 +5,19 @@ from chromesy import config
 
 def get_chrome_user_folder(user=None):
     home_directory = os.path.expanduser(user)
-    return "/" + os.path.join(home_directory, config.CHROME_WINDOWS_PATH)
+    return os.path.join(home_directory, config.CHROME_WINDOWS_PATH)
 
 
 def get_chrome_history_path(user=config.DEFAULT_USER):
-    return os.path.join(get_chrome_user_folder(user), config.HISTORY_FILE)
+    return "/" + os.path.join(get_chrome_user_folder(user), config.HISTORY_FILE)
 
 
 def get_chrome_logins_path(user=config.DEFAULT_USER):
-    return os.path.join(get_chrome_user_folder(user), config.LOGINS_FILE)
+    return "/" + os.path.join(get_chrome_user_folder(user), config.LOGINS_FILE)
 
 
 def get_chrome_top_sites_path(user=config.DEFAULT_USER):
-    return os.path.join(get_chrome_user_folder(user), config.TOP_SITES_FILE)
+    return "/" + os.path.join(get_chrome_user_folder(user), config.TOP_SITES_FILE)
 
 
 def get_chrome_profile_picture_path(user=config.DEFAULT_USER):

5ad54f933e287742c79153a5f0e0c14c8df67b8f
ben <ben.gabay38@gmail.com>
2020-09-04 17:20:24 +0300

    fix path methods

--- chromesy/cli.py
+++ chromesy/cli.py
@@ -13,6 +13,7 @@ from sqlalchemy.exc import OperationalError
 from . import config
 from .arg_parser import create_arg_parser
 from .chrome import ChromeDataAdapter
+from .config import DESTINATION_FOLDER
 from .dal.table_adapters.history import HistoryTableAdapter
 from .dal.table_adapters.logins import LoginsTableAdapter
 from .dal.table_adapters.top_sites import TopSitesTableAdapter
@@ -21,19 +22,32 @@ from .file_adapters.json_adapter import JsonFileAdapter
 from .path import get_chrome_logins_path, get_chrome_history_path, get_chrome_top_sites_path
 
 
+def export_chrome_data(chrome_data_adapter, user):
+    if not os.path.exists(DESTINATION_FOLDER):
+        os.mkdir(DESTINATION_FOLDER)
+    chrome_data_adapter.export_credentials(f"{DESTINATION_FOLDER}/credentials.csv")
+    chrome_data_adapter.export_history(f"{DESTINATION_FOLDER}/history.csv")
+    chrome_data_adapter.export_profile_picture(user, f"{DESTINATION_FOLDER}/profile.jpg")
+    chrome_data_adapter.export_top_sites(f"{DESTINATION_FOLDER}/top_sites.csv")
+    chrome_data_adapter.export_downloads(f"{DESTINATION_FOLDER}/downloads.csv")
+
+
+def import_chrome_data(chrome_data_adapter):
+    pass
+
+
 def main():
     arg_parser = create_arg_parser()
     args = arg_parser.parse_args()
-    user = "~"
 
     csv_file_adapter = CsvFileAdapter()
     logins_table_adapter = LoginsTableAdapter()
     history_table_adapter = HistoryTableAdapter()
     top_sites_table_adapter = TopSitesTableAdapter()
 
-    logins_table_adapter.connect(config.DB_PROTOCOL, get_chrome_logins_path(user))
-    history_table_adapter.connect(config.DB_PROTOCOL, get_chrome_history_path(user))
-    top_sites_table_adapter.connect(config.DB_PROTOCOL, get_chrome_top_sites_path(user))
+    logins_table_adapter.connect(config.DB_PROTOCOL, get_chrome_logins_path(args.user))
+    history_table_adapter.connect(config.DB_PROTOCOL, get_chrome_history_path(args.user))
+    top_sites_table_adapter.connect(config.DB_PROTOCOL, get_chrome_top_sites_path(args.user))
 
     chrome_data_adapter = ChromeDataAdapter(
         csv_file_adapter,
@@ -41,15 +55,10 @@ def main():
         history_table_adapter,
         top_sites_table_adapter
     )
-
-    if not os.path.exists("dist"):
-        os.mkdir("dist")
     try:
         if args.mode == "export":
-            chrome_data_adapter.export_credentials("dist/credentials.csv")
-            chrome_data_adapter.export_history("dist/history.csv")
-            chrome_data_adapter.export_profile_picture("~", "dist/profile.jpg")
-            chrome_data_adapter.export_top_sites("dist/top_sites.csv")
-            chrome_data_adapter.export_downloads("dist/downloads.csv")
+            export_chrome_data(chrome_data_adapter, args.user)
+        elif args.mode == "import":
+            import_chrome_data(chrome_data_adapter)
     except OperationalError as e:
         print("chrome database is locked. close chrome and try again")
--- chromesy/path.py
+++ chromesy/path.py
@@ -1,24 +1,31 @@
 import os
 
 from chromesy import config
+from chromesy.utils.exceptions.chrome_not_installed_exception import ChromeNotInstalledException
+from chromesy.utils.exceptions.user_not_found_exception import UserNotFoundException
 
 
-def get_chrome_user_folder(user=None):
-    home_directory = os.path.expanduser(user)
-    return os.path.join(home_directory, config.CHROME_WINDOWS_PATH)
+def get_chrome_user_folder(user):
+    home_directory = os.path.expanduser("~" + user)
+    if not os.path.exists(home_directory):
+        raise UserNotFoundException(user)
+    chrome_user_folder = os.path.join(home_directory, config.CHROME_WINDOWS_PATH)
+    if not not os.path.exists(chrome_user_folder):
+        raise ChromeNotInstalledException(user)
+    return chrome_user_folder
 
 
-def get_chrome_history_path(user=config.DEFAULT_USER):
+def get_chrome_history_path(user):
     return "/" + os.path.join(get_chrome_user_folder(user), config.HISTORY_FILE)
 
 
-def get_chrome_logins_path(user=config.DEFAULT_USER):
+def get_chrome_logins_path(user):
     return "/" + os.path.join(get_chrome_user_folder(user), config.LOGINS_FILE)
 
 
-def get_chrome_top_sites_path(user=config.DEFAULT_USER):
+def get_chrome_top_sites_path(user):
     return "/" + os.path.join(get_chrome_user_folder(user), config.TOP_SITES_FILE)
 
 
-def get_chrome_profile_picture_path(user=config.DEFAULT_USER):
+def get_chrome_profile_picture_path(user):
     return os.path.join(get_chrome_user_folder(user), config.GOOGLE_PICTURE_FILE)

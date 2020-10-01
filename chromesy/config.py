ab7a3b4c304b0499d50cf233329ca3f176219a06
ben <ben.gabay38@gmail.com>
2020-10-02 00:57:54 +0300

    replace all ' to double quotes in config file

--- chromesy/config.py
+++ chromesy/config.py
@@ -4,10 +4,10 @@ HISTORY_FILE = "History"
 TOP_SITES_FILE = "Top Sites"
 GOOGLE_PICTURE_FILE = "Google Profile Picture.png"
 
-OUTPUT_CREDENTIALS_FILE = 'passwords.csv'
-OUTPUT_HISTORY_FILE = 'history.csv'
-OUTPUT_DOWNLOADS_FILE = 'downloads.csv'
-OUTPUT_TOP_SITES_FILE = 'top_sites.csv'
+OUTPUT_CREDENTIALS_FILE = "passwords.csv"
+OUTPUT_HISTORY_FILE = "history.csv"
+OUTPUT_DOWNLOADS_FILE = "downloads.csv"
+OUTPUT_TOP_SITES_FILE = "top_sites.csv"
 OUTPUT_PROFILE_PICTURE_FILE = "profile.jpg"
 DEFAULT_EXPORT_DESTINATION_FOLDER = "dist"
 DEFAULT_USER = "~"

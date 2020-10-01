4aaba9e6d5aca30bab03ca269f7b6a3a0abbbf23
ben <ben.gabay38@gmail.com>
2020-10-02 01:10:13 +0300

    change config variable names

--- chromesy/config.py
+++ chromesy/config.py
@@ -1,7 +1,10 @@
 CHROME_WINDOWS_PATH = r"AppData\Local\Google\Chrome\User Data\Default"
-LOGINS_FILE = "Login Data"
-HISTORY_FILE = "History"
-TOP_SITES_FILE = "Top Sites"
+CHROME_LINUX_PATH = ".config/google-chrome/Default/databases"
+CHROME_MAC_OS_PATH = "Library/Application Support/Google/Chrome"
+
+LOGINS_DB_FILE = "Login Data"
+HISTORY_DB_FILE = "History"
+TOP_SITES_DB_FILE = "Top Sites"
 GOOGLE_PICTURE_FILE = "Google Profile Picture.png"
 
 OUTPUT_CREDENTIALS_FILE = "passwords.csv"
@@ -10,7 +13,8 @@ OUTPUT_DOWNLOADS_FILE = "downloads.csv"
 OUTPUT_TOP_SITES_FILE = "top_sites.csv"
 OUTPUT_PROFILE_PICTURE_FILE = "profile.jpg"
 DEFAULT_EXPORT_DESTINATION_FOLDER = "dist"
-DEFAULT_USER = "~"
 
 DB_PROTOCOL = "sqlite"
+
+DEFAULT_USER = "~"
 WRITE_FILE_MODE = "w"
--- chromesy/services/path.py
+++ chromesy/services/path.py
@@ -1,6 +1,6 @@
 import os
 
-from chromesy.config import CHROME_WINDOWS_PATH, HISTORY_FILE, LOGINS_FILE, TOP_SITES_FILE, GOOGLE_PICTURE_FILE
+from chromesy.config import CHROME_WINDOWS_PATH, HISTORY_DB_FILE, LOGINS_DB_FILE, TOP_SITES_DB_FILE, GOOGLE_PICTURE_FILE
 from chromesy.utils.exceptions.ChromeNotInstalledException import ChromeNotInstalledException
 from chromesy.utils.exceptions.UserNotFoundException import UserNotFoundException
 
@@ -16,15 +16,15 @@ def get_chrome_user_folder(user: str) -> str:
 
 
 def get_chrome_history_path(user: str) -> str:
-    return "/" + os.path.join(get_chrome_user_folder(user), HISTORY_FILE)
+    return "/" + os.path.join(get_chrome_user_folder(user), HISTORY_DB_FILE)
 
 
 def get_chrome_logins_path(user: str) -> str:
-    return "/" + os.path.join(get_chrome_user_folder(user), LOGINS_FILE)
+    return "/" + os.path.join(get_chrome_user_folder(user), LOGINS_DB_FILE)
 
 
 def get_chrome_top_sites_path(user: str) -> str:
-    return "/" + os.path.join(get_chrome_user_folder(user), TOP_SITES_FILE)
+    return "/" + os.path.join(get_chrome_user_folder(user), TOP_SITES_DB_FILE)
 
 
 def get_chrome_profile_picture_path(user: str) -> str:

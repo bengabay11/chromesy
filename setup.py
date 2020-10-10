4a86dcb16898e08def0d504dd7bd6d14a554a625
ben <ben.gabay38@gmail.com>
2020-10-10 21:05:34 +0300

    add license, description and  to install_requires in setup.py file

--- setup.py
+++ setup.py
@@ -10,13 +10,15 @@ setuptools.setup(
     version=__version__,
     author="Ben Gabay",
     author_email="ben.gabay38@gmail.com",
-    description="",
+    license="License :: OSI Approved :: MIT License",
+    description="export and import chrome passwords",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bengabay11/chpass",
     packages=setuptools.find_packages(),
     install_requires=[
-        "sqlalchemy==1.3.18"
+        "sqlalchemy==1.3.18",
+        "pandas==1.1.2"
     ],
     entry_points={
         'console_scripts': ['chpass = chpass.__init__:main'],

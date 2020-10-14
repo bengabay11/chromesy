9fda56f1d27792ca83df47acf6cb5b8a334e5cb6
ben <ben.gabay38@gmail.com>
2020-10-14 15:17:26 +0300

    fix annotations for csv file adapter

--- chpass/services/file_adapters/csv.py
+++ chpass/services/file_adapters/csv.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pandas as pd
 
 from chpass.config import CSV_ADAPTER_ENCODING
@@ -5,10 +7,10 @@ from chpass.services.interfaces.IFileAdapter import IFileAdapter
 
 
 class CsvFileAdapter(IFileAdapter):
-    def write(self, data_list: list, output_file_path: str) -> None:
+    def write(self, data_list: List[dict], output_file_path: str) -> None:
         df = pd.DataFrame(data_list)
         df.to_csv(output_file_path, index=False, encoding=CSV_ADAPTER_ENCODING)
 
-    def read(self, path: str, converters: dict) -> list:
+    def read(self, path: str, converters: dict = None) -> list:
         df = pd.read_csv(path, encoding=CSV_ADAPTER_ENCODING, converters=converters)
         return [dict(row[1]) for row in df.iterrows()]

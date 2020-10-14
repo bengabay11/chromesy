f8641b906acb86cf4879843af14f20449215c6d2
ben <ben.gabay38@gmail.com>
2020-10-14 18:09:59 +0300

    add fixture for correct destination profile picture path

--- tests/unit/test_profile_picture.py
+++ tests/unit/test_profile_picture.py
@@ -7,28 +7,30 @@ from chpass.exceptions.UserNotFoundException import UserNotFoundException
 from chpass.services.profile_picture import export_profile_picture
 
 
-def test_export_profile_picture():
+@pytest.fixture(scope="module")
+def correct_destination_path() -> str:
+    return "test_profile_picture.jpg"
+
+
+def test_export_profile_picture(correct_destination_path):
     user = getpass.getuser()
-    destination_path = "test_profile_picture.jpg"
-    export_profile_picture(user, destination_path)
-    assert os.path.exists(destination_path)
-    os.remove(destination_path)
+    export_profile_picture(user, correct_destination_path)
+    assert os.path.exists(correct_destination_path)
+    os.remove(correct_destination_path)
 
 
-def test_export_profile_picture_user_not_exist():
+def test_export_profile_picture_user_not_exist(correct_destination_path):
     with pytest.raises(UserNotFoundException):
         user = "not_exist"
-        destination_path = "test_profile_picture.jpg"
-        export_profile_picture(user, destination_path)
-    assert not os.path.exists(destination_path)
+        export_profile_picture(user, correct_destination_path)
+    assert not os.path.exists(correct_destination_path)
 
 
-def test_export_profile_picture_invalid_user():
+def test_export_profile_picture_invalid_user(correct_destination_path):
     with pytest.raises(TypeError):
         user = -1
-        destination_path = "test_profile_picture.jpg"
-        export_profile_picture(user, destination_path)
-    assert not os.path.exists(destination_path)
+        export_profile_picture(user, correct_destination_path)
+    assert not os.path.exists(correct_destination_path)
 
 
 def test_export_profile_picture_destination_path_not_exist():

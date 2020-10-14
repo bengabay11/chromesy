7e695b219e77b091f668c4fbd847fd351cfdd613
ben <ben.gabay38@gmail.com>
2020-10-14 23:43:05 +0300

    add empty tests for integration

---
+++ tests/integration/test_export.py
@@ -0,0 +1,24 @@
+
+
+def test_default_export(export_mode):
+    pass
+
+
+def test_export_with_current_user(export_mode, current_user):
+    pass
+
+
+def test_export_with_disconnected_user(export_mode, disconnected_user):
+    pass
+
+
+def test_export_file_adapter(export_mode, file_adapter_type):
+    pass
+
+
+def test_export_all_data(export_mode):
+    pass
+
+
+def test_export_destination_folder(export_mode, destination_folder):
+    pass
---
+++ tests/integration/test_import.py
@@ -0,0 +1,16 @@
+
+
+def test_default_import(import_mode, from_file):
+    pass
+
+
+def test_import_with_current_user(import_mode, from_file, current_user):
+    pass
+
+
+def test_import_with_disconnected_user(import_mode, from_file, disconnected_user):
+    pass
+
+
+def test_export_file_adapter(import_mode, from_file, file_adapter_type):
+    pass
